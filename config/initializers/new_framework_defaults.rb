75162b5c973aa01763a905a74a8358dbefd61c1e
EC2 Default User <ec2-user@ip-172-31-39-158.us-east-2.compute.internal>
2019-06-16 09:50:47 +0000

    modify new_framework_defaults for heroku error

--- config/initializers/new_framework_defaults.rb
+++ config/initializers/new_framework_defaults.rb
@@ -6,7 +6,8 @@
 #
 # Read the Guide for Upgrading Ruby on Rails for more info on each option.
 
-Rails.application.config.action_controller.raise_on_unfiltered_parameters = true
+#以下の行は、heroku上のアプリエラー"Invalid option key: raise_on_unfiltered_parameters= (RuntimeError)"の対策のためにコメントアウトしました
+#Rails.application.config.action_controller.raise_on_unfiltered_parameters = true
 
 # Enable per-form CSRF tokens. Previous versions had false.
 Rails.application.config.action_controller.per_form_csrf_tokens = false
